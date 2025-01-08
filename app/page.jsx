5011edfc2830cfbb4d3228605da87d5e13f47678
Piyush2604 <khattrapiyush80@gmail.com>
2025-01-08 23:08:27 +0530

    updated resume

--- app/page.jsx
+++ app/page.jsx
@@ -21,7 +21,7 @@ const Home = () => {
             {/* btn and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <a
-                href="/Piyush_s_Resume.pdf"
+                href="/PiyushS' RESUME.pdf"
                 download="Piyush_Khattar_CV.pdf"
               >
                 <Button
